ccf243e53baf40daad9b6485a5f25958ddb1f254
徐庆臣 <19655910@qq.com>
2018-12-04 23:09:46 +0800

    Initial commit

---
+++ app/src/main/java/com/kson/ksonheart/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kson.ksonheart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kson.ksonheart.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
